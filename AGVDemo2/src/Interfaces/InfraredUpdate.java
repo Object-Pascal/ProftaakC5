cefb8c0fa7156c38834db233d51a227e569216c8
Object-Pascal <passiboy@live.nl>
2018-12-06 15:02:45 +0100

    Revert "Revert "Merge branch 'master' of https://github.com/Object-Pascal/ProftaakC5""
    This reverts commit 40611835f977040256947a0d5bee329cf80348e2.

---
+++ AGVDemo2/src/Interfaces/InfraredUpdate.java
@@ -0,0 +1,5 @@
+package Interfaces;
+
+public interface InfraredUpdate {
+    void onInfraredUpdate(int value);
+}
---
+++ AGVDemo2/src/Interfaces/UltrasoneUpdate.java
@@ -0,0 +1,5 @@
+package Interfaces;
+
+public interface UltrasoneUpdate {
+    void onUltrasoneUpdate(int value);
+}
